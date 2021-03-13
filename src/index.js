2091eba770cd9da5ec7648327bdb1b57ebd4f80b
Antoine Boulanger <hey@antoineboulanger.com>
2021-03-13 13:03:21 -0500

    Add serveOptions

--- src/index.js
+++ src/index.js
@@ -7,44 +7,47 @@ import postcssCustomMedia from 'postcss-custom-media';
 
 export const isBuild = !isWatch;
 
-const build = (options = {}) => {
+const build = (options = {}, serveOptions = {}) => {
 	const { plugins = [] } = options;
 
-	esbuildServe({
-		entryPoints: ['src/index.js'],
-		outfile: 'public/scripts/main.js',
-		bundle: true,
-		format: 'esm',
-		target: 'es2017',
-		sourcemap: isWatch,
-		minify: isBuild,
-		...options,
-		plugins: [
-			pipe({
-				plugins: [
-					babel({
-						config: {
-							sourceMaps: isWatch && 'inline',
-							plugins: [
-								[
-									'@babel/plugin-proposal-decorators',
-									{ decoratorsBeforeExport: true }
-								],
-								'@babel/plugin-proposal-class-properties'
-							]
-						}
-					}),
-					postcssLiteral({
-						minify: isBuild,
-						config: {
-							plugins: [postcssNesting(), postcssCustomMedia()]
-						}
-					})
-				]
-			}),
-			...plugins
-		]
-	});
+	esbuildServe(
+		{
+			entryPoints: ['src/index.js'],
+			outfile: 'public/scripts/main.js',
+			bundle: true,
+			format: 'esm',
+			target: 'es2017',
+			sourcemap: isWatch,
+			minify: isBuild,
+			...options,
+			plugins: [
+				pipe({
+					plugins: [
+						babel({
+							config: {
+								sourceMaps: isWatch && 'inline',
+								plugins: [
+									[
+										'@babel/plugin-proposal-decorators',
+										{ decoratorsBeforeExport: true }
+									],
+									'@babel/plugin-proposal-class-properties'
+								]
+							}
+						}),
+						postcssLiteral({
+							minify: isBuild,
+							config: {
+								plugins: [postcssNesting(), postcssCustomMedia()]
+							}
+						})
+					]
+				}),
+				...plugins
+			]
+		},
+		serveOptions
+	);
 };
 
 export default build;
